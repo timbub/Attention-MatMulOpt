36422e9e8c4eddff9011dd19cf3f5ee53f5401e0
Tim Surkov <timasurckov@gmail.com>
2026-04-08 16:09:50 +0300

    Initial commit

---
+++ Tensor.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "BufMatrix.hpp"
+#include "Matrix.hpp"
+
+namespace tensor {
+    template <typename ElemT> class Tensor {
+    private:
+        size_t batch_size_;
+        size_t seq_len_;
+        size_t dim_;
+        matrix::BufMatrix<ElemT> data_;
+    public:
+        Tensor(size_t b, size_t s, size_t d) : batch_size_(b), seq_len_(s), dim_(d) {}
+        Tensor(const Tensor& other)            = delete;
+        Tensor& operator=(const Tensor& other) = delete;
+
+        matrix::Matrix<ElemT> get_batch(size_t idx) const {
+            return matrix::Matrix(seq_len_, dim_, data_.get_row_ptr(idx*seq_len, dim));
+        }
+
+        size_t  get_batch_size() const {return batch_size_};
+        size_t  get_seq_len()    const {return seq_len};
+        size_t  get_dim()        const {return dim};
+    };
+};
+
